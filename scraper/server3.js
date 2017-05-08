4ef0458b49529da70b2ebff2d8d03f9c352e7c47
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-07 21:20:47 -0500

    first 105 restaurants reviewed scraper

--- scraper/server3.js
+++ scraper/server3.js
@@ -8,7 +8,7 @@ const {getReviewsOnPage} = require('./reviewScrape');
 const {lineBreakRemover, parseCount, getRatingNumber, reviewPages, lastPageReviews } = require('./helper.js');
 const restaurantJson = require('./restaurantsJson.js')
 
-let j = 83;
+let j = 85;
 
 
 
