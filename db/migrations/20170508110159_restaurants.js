84388ba1e24f33000384fe5cb9952547c5d041a5
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-08 13:12:37 -0500

    restaurant seeds run

--- db/migrations/20170508110159_restaurants.js
+++ db/migrations/20170508110159_restaurants.js
@@ -3,13 +3,14 @@ exports.up = function(knex, Promise) {
   return knex.schema
   .createTableIfNotExists('restaurants', function (table) {
     table.string('yelp_id').notNullable().unique().primary();
+    table.string('name');
     table.string('price_range');
-    table.specificType('category_str_list', knex.raw('text[]'));
+    table.string('category_str_list');
     table.string('address_string');
     table.string('address_city');
     table.string('address_state');
     table.string('address_zip');
-    table.specificType('neighborhood_str_list', knex.raw('text[]'));
+    table.string('neighborhood_str_list');
     table.string('website');
     table.string('phone');
     table.string('monday_hours');
