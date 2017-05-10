75dee58ab8c5c14efdd7151603bf97505bb8edf1
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-10 10:39:39 -0500

    have whole dataset

--- db/seeds/development/b_users_seed.js
+++ db/seeds/development/b_users_seed.js
@@ -2,29 +2,19 @@
 
 const {knex} = require('../../database');
 const {getUniqueUsers, compareId} = require('../../../scraper/helper.js')
-const reviews = require('../../../sortedUsers');
+const users = require('../../data/users.json');
 
-// const reviews = require('./reviewsJson.json')
-// console.log(reviews[1]);
-console.log("number of reviews", reviews.length)
 
-// let sortedUsers = getUniqueUsers(reviews);
-// // console.log("sortedUsers",sortedUsers)
-// let uniqueUsers = compareId(sortedUsers);
-// console.log("uniqueUsers", uniqueUsers);
-const reviewPromise = reviews.map(
+console.log("number of users", users.length)
+
+
+const reviewPromise = users.map(
   ({yelp_id,
     user_name,
     user_location}) => {
-    // console.log("yelp_id", yelp_id);
-    // console.log("user_name", user_name);
-    // console.log("user_location", user_location)
+
     return knex('users').insert({id: yelp_id, user_name, user_location})
 
-  // catch (e) {
-  //   console.log("catch")
-  //   console.log(e);
-  // }
 })
 
 exports.seed = function(knex, Promise) {
