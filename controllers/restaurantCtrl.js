15d999c0d760dc1e3b61b13d4de161d9db59ad17
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-11 15:00:05 -0500

    returns users by rst review rating

--- controllers/restaurantCtrl.js
+++ controllers/restaurantCtrl.js
@@ -24,27 +24,3 @@ module.exports.getSingleRestaurant = ({params: {id}}, res, next) => {
     next(error);
   })
 }
-
-
-// module.exports.getReviewsByRestaurant = ({query: {restaurantId}}, res, next) => {
-//   Restaurant.query(`where`, )
-//   .fetch({withRelated: ['reviews'], require: true})
-//   .then((restReviews) => {
-//     res.status(200).json(restReviews)
-//   })
-//   .catch((error) => {
-//     next(error);
-//   })
-// }
-
-//SQL query to get users by a certain restaurant and their rating
-// select users.user_name, reviews.user_id, reviews.restaurant_id, reviews.rating from users
-// join reviews on users.id = reviews.user_id
-// where reviews.restaurant_id = 'the-catbird-seat-nashville' and reviews.rating <= 2
-// order by reviews.rating;
-
-//get all users who reviewed a restaurant a certain way
-
-// module.exports.getUsersByRestaurantRating = ({query: {restaurantId}, {rating}}, res, next) => {
-//   Restaurant.forge({id: restaurantId})
-// }
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -6,27 +6,27 @@ const User = require('../models/userModel');
 const Review = require('../models/reviewModel')
 
 
-// module.exports.getUsersByRestaurantRating = ({query: {restaurantId}}, res, next) => {
-//    //Restaurant.query({where: {id : restaurantId} and {rating: rating}})
-//    // Restaurant.where({id : query.restaurantId})
-//    console.log('query', restaurantId)
-//   Restaurant.forge({id : 'the-catbird-seat-nashville'})
-//   .fetch({withRelated: ['users'], require: true}).where()
-//   .then((users) => {
-//     res.status(200).json(users)
-//   })
-//   .catch((error) => {
-//     next(error);
-//   })
-// }
+//SQL query to get users by a certain restaurant and their rating
+// select users.user_name, reviews.user_id, reviews.restaurant_id, reviews.rating from users
+// join reviews on users.id = reviews.user_id
+// where reviews.restaurant_id = 'the-catbird-seat-nashville' and reviews.rating <= 2
+// order by reviews.rating;
 
+//get all users who reviewed a restaurant a certain way
+
+
+
+const getUsersByRestaurantRatingKnex = (restaurant_id, rating) => {
+  return knex.raw(`select * from users
+join reviews on users.id = reviews.user_id
+where reviews.restaurant_id = '${restaurant_id}' and reviews.rating ${rating}
+order by reviews.rating;`)
+}
 
 module.exports.getUsersByRestaurantRating = ({query}, res, next) => {
-   //Restaurant.query({where: {id : restaurantId} and {rating: rating}})
-   // Restaurant.where({id : query.restaurantId})
-   console.log('query', query.restaurantId)
-  Restaurant.forge({id : 'the-catbird-seat-nashville'})
-  .fetch({withRelated: ['users'], require: true})
+  let restaurant_id = query.restaurant_id;
+  let rating = query.rating;
+  getUsersByRestaurantRatingKnex(restaurant_id, rating)
   .then((users) => {
     res.status(200).json(users)
   })
