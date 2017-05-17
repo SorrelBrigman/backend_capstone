29857bf2d285c50b1689d16912f22033792613bc
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-17 12:57:44 -0500

    commented out 2 produciton seed files

--- db/seeds/production/b_users_seed.js
+++ db/seeds/production/b_users_seed.js
@@ -1,28 +1,28 @@
-'use strict'
+// 'use strict'
 
-const {knex} = require('../../database');
-const {getUniqueUsers, compareId} = require('../../../scraper/helper.js')
-const users = require('../../data/users.json');
-
-
-console.log("number of users", users.length)
+// const {knex} = require('../../database');
+// const {getUniqueUsers, compareId} = require('../../../scraper/helper.js')
+// const users = require('../../data/users.json');
 
 
-const reviewPromise = users.map(
-  ({yelp_id,
-    user_name,
-    user_location}) => {
+// console.log("number of users", users.length)
 
-    return knex('users').insert({id: yelp_id, user_name, user_location})
 
-})
+// const reviewPromise = users.map(
+//   ({yelp_id,
+//     user_name,
+//     user_location}) => {
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(() => {
-      // Inserts seed entries
-      return Promise.all(reviewPromise)
-      .catch(e => {console.log("my error", e)})
-    });
-};
+//     return knex('users').insert({id: yelp_id, user_name, user_location})
+
+// })
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(() => {
+//       // Inserts seed entries
+//       return Promise.all(reviewPromise)
+//       .catch(e => {console.log("my error", e)})
+//     });
+// };
