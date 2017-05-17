67ce6cca839cf87b868784fc076d2b4a1a447ac0
Sorrel Brigman <sorrel.k@gmail.com>
2017-05-17 13:02:24 -0500

    moved seed files out of folder

---
+++ db/seeds/c_reviews_seed.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const {knex} = require('../../database');
+
+const reviews = require('../../data/reviews.json');
+
+
+console.log("number of reviews", reviews.length)
+
+// let sortedUsers = getUniqueUsers(reviews);
+// // console.log("sortedUsers",sortedUsers)
+// let uniqueUsers = compareId(sortedUsers);
+// console.log("uniqueUsers", uniqueUsers);
+const reviewPromise = reviews.map(
+  ({restaurant_id,
+    yelp_id,
+    rating,
+    review_date,
+    votes_useful,
+    votes_funny,
+    votes_cool}) => {
+    // console.log("yelp_id", yelp_id);
+    // console.log("user_name", user_name);
+    // console.log("user_location", user_location)
+    return knex('reviews').insert({
+      user_id: yelp_id,
+      restaurant_id,
+      rating,
+      review_date,
+      votes_useful,
+      votes_funny,
+      votes_cool
+    })
+
+  // catch (e) {
+  //   console.log("catch")
+  //   console.log(e);
+  // }
+})
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('reviews').del()
+    .then(() => {
+      // Inserts seed entries
+      return Promise.all(reviewPromise)
+      .catch(e => {console.log("my error", e)})
+    });
+};
